3b707e1fe145fd362998cade7b5044e39faf8677
AndresEsparzaALU <3481343@alu.murciaeduca.es>
2023-01-26 12:48:06 +0100

    Ahora muestra informacion de los vehiculos disponibles

---
+++ src/components/react-admin/bicis.js
@@ -0,0 +1,75 @@
+// in src/components/react-admin/bicis.js
+import {
+    List,
+    SimpleList,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    TextInput,
+    NumberField,
+    ImageField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    NumberInput
+  } from 'react-admin';
+
+import { useRecordContext} from 'react-admin';
+import { useMediaQuery } from '@mui/material';
+
+const biciFilters = [
+    <TextInput source="q" label="Search" alwaysOn />
+];
+
+export const BiciList = () => {
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down('sm'));
+  return (
+    <List filters={biciFilters} >
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.tipo}
+          secondaryText={(record) => record.descripcion}
+          tertiaryText={(record) => record.imagen}
+          linkType={(record) => 'show'}
+        >
+          <EditButton />
+        </SimpleList>
+      ) : (
+        <Datagrid bulkActionButtons={false} >
+          <TextField source="tipo" />
+          <TextField source="descripcion" />
+          <ImageField source="imagen" title="descripcion"/>
+          <EditButton />
+        </Datagrid>
+      )}
+    </List>
+  );
+}
+
+export const BiciCreate = () => (
+  <Create>
+      <SimpleForm>
+        <TextInput source="tipo" />
+        <TextInput source="descripcion" />
+        <TextInput source="imagen" />
+      </SimpleForm>
+  </Create>
+  );
+
+  const BiciTitle = () => {
+    const record = useRecordContext();
+    return <span>Vehiculo {record ? `"${record.tipo} ${record.descripcion}"` : ''}</span>;
+  };
+  
+  export const BiciEdit = () => (
+      <Edit title={<BiciTitle />}>
+      <SimpleForm>
+          <TextInput source="id" disabled />
+          <TextInput source="tipo" />
+          <TextInput source="descripcion" />
+          <TextInput source="imagen" />
+      </SimpleForm>
+      </Edit>
+  );
