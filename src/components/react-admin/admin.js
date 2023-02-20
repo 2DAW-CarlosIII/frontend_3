84d958ff64b0aab3cc75591697772110ddc1145f
AndresEsparzaALU <3481343@alu.murciaeduca.es>
2023-02-20 11:37:22 +0100

    Ahora se puede añadir vehiculo y su tipo

--- src/components/react-admin/admin.js
+++ src/components/react-admin/admin.js
@@ -5,30 +5,21 @@ import jsonapiClient from 'ra-jsonapi-client';
 import { UserList } from 'components/react-admin/users';
 import { CustomerList, CustomerEdit, CustomerCreate } from 'components/react-admin/customers';
 
-//TODO eliminar las dos lineas siguientes
-import { PostList, PostEdit, PostCreate } from 'components/react-admin/posts';
-import { MigrationList, MigrationEdit, MigrationCreate } from 'components/react-admin/migrations';
 import { StationList } from 'components/react-admin/stations';
 import { BiciList, BiciCreate, BiciEdit } from 'components/react-admin/bicis';
+import { TipoVehiculoList, TipoVehiculoCreate, TipoVehiculoEdit } from 'components/react-admin/tipovehiculo';
 
-import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
-import MigrationIcon from '@mui/icons-material/Storage';
 import CustomerIcon from '@mui/icons-material/SupportAgent';
 import StationIcon from '@mui/icons-material/EvStation';
 import BiciIcon from '@mui/icons-material/PedalBike';
+import TipoVehiculoIcon from '@mui/icons-material/ElectricBikeOutlined'
 
 import { AdminLayout } from 'components/react-admin/adminLayout';
 
 import { default as AuthProvider } from 'components/react-admin/authProvider';
 import { default as Login } from 'pages/login'
 
-//PARA PHP CRUD API
-//
-//const dataProvider = jsonServerProvider('http://encuentro.test/api/records');
-
-//PARA LARAVEL CONTROLLERS
-//
 const dataProvider = jsonapiClient('http://encuentro.test/api');
 
 const RAdmin = () => {
@@ -54,11 +45,10 @@ const RAdmin = () => {
     >
       <Resource name="customers" list={CustomerList} icon={CustomerIcon}
         edit={CustomerEdit} create={CustomerCreate} />
-      <Resource name="migrations"
-        list={MigrationList} icon={MigrationIcon} edit={MigrationEdit} create={MigrationCreate} />
-      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
       <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
       <Resource name="stations" list={StationList} icon={StationIcon} />
+      <Resource name="tipoVehiculos" list={TipoVehiculoList} icon={TipoVehiculoIcon}
+      create={TipoVehiculoCreate} edit={TipoVehiculoEdit}/>
       <Resource name="vehiculos" list={BiciList} icon={BiciIcon} create={BiciCreate}
         edit={BiciEdit} />
     </Admin>
